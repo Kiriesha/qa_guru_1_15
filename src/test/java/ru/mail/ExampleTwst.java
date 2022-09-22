767e30139b2010692293e0c36775f46ceff9181d
stasy <stasya3354@mail.ru>
2022-09-22 21:48:50 +0300

    init commit

---
+++ src/test/java/ru/mail/ExampleTwst.java
@@ -0,0 +1,22 @@
+package ru.mail;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTwst {
+    @Test
+    void exampleTest0() {
+        Assertions.assertFalse(2 <= 1);
+    }
+
+    @Test
+    void exampleTest1() {
+        Assertions.assertTrue(3 >= 2);
+    }
+
+    @Test
+    void exampleTest2(){
+        Assertions.assertFalse(10>=11);
+    }
+}
+
