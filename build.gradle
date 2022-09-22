0d6c1a678a821d6002209a4502553b4c218a83ae
stasy <stasya3354@mail.ru>
2022-09-22 21:27:28 +0300

    init commit

---
+++ build.gradle
@@ -0,0 +1,19 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+public class ExampleTest {
+
+    @Test
+    void exampleTest0() {
+        Assertions.assertFalse(2 <= 1);
+    }
+
+    @Test
+    void exampleTest1() {
+        Assertions.assertTrue(3 >= 2);
+    }
+}
